3ec20f4fa145d23462c7afdd98f56b704ca17910
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-02-07 08:45:49 -0500

    Need to fix...

---
+++ frq-solutions/ArrayResizer.java
@@ -0,0 +1,22 @@
+public class ArrayResizer{
+  pulic static void main(String[] args){
+    
+
+  }//end main
+  public static boolean isNonZeroRow(int[][]array2D,int r){
+    for(int x : array2D[r]){
+      for(x==0){
+        return "false";
+      }
+      if else{
+        return "true"
+      }
+    }
+  }//end isNonZeroRow
+  public static int[][] array2D){
+    int numRow =array2D.length;
+    intCols = array2D[0].length;
+
+
+  }
+}end class
