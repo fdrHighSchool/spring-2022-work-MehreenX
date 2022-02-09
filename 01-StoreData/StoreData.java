f9e2db927408a215dc9382d1fcbff39485983518
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-02-08 23:24:52 -0500

    Finished.

---
+++ 01-StoreData/StoreData.java
@@ -0,0 +1,64 @@
+public class StoreData{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    //Student number 1
+
+    System.out.println("Please enter the following information for Student #1");
+    System.out.println("Enter ID");
+    String id_1 = sc.nextLine();
+    System.out.println("Enter name");
+    String name_1 = sc.nextLine();
+    System.out.println("Enter Close Contact (Y/N) ");
+    String contact_1 = sc.nextLine();
+    System.out.println("Enter each grade followed by commas");
+    String grades_1 = sc.nextLine();
+
+    //Student number 2
+
+    System.out.println("Please enter the following information for Student #2");
+    System.out.println("Enter ID");
+    String id_2 = sc.nextLine();
+    System.out.println("Enter name");
+    String name_2 = sc.nextLine();
+    System.out.println("Enter Close Contact (Y/N) ");
+    String contact_2 = sc.nextLine();
+    System.out.println("Enter each grade followed by commas");
+    String grades_2 = sc.nextLine();
+
+    //Student number 3
+
+    System.out.println("Please enter the following information for Student #2");
+    System.out.println("Enter ID");
+    String id_3 = sc.nextLine();
+    System.out.println("Enter name");
+    String name_3 = sc.nextLine();
+    System.out.println("Enter Close Contact (Y/N) ");
+    String contact_3 = sc.nextLine();
+    System.out.println("Enter each grade followed by commas");
+    String grades_3 = sc.nextLine();
+
+    //Student number 4
+
+    System.out.println("Please enter the following information for Student #2");
+    System.out.println("Enter ID");
+    String id_4 = sc.nextLine();
+    System.out.println("Enter name");
+    String name_4 = sc.nextLine();
+    System.out.println("Enter Close Contact (Y/N) ");
+    String contact_4 = sc.nextLine();
+    System.out.println("Enter each grade followed by commas");
+    String grades_4 = sc.nextLine();
+
+
+    System.out.println("Student ID: " + id_1 + " , " +  name_1 + " , " +  contact_1 + " , " +  "Grades: " + grades_1);
+
+    System.out.println("Student ID: " + id_2 + " , " +  name_2 + " , " +  contact_2 + " , " +  "Grades: " + grades_2);
+
+    System.out.println("Student ID: " + id_3 + " , " +  name_3 + " , " +  contact_3 + " , " +  "Grades: " + grades_3);
+
+    System.out.println("Student ID: " + id_4 + " , " +  name_4 + " , " +  contact_4 + " , " +  "Grades: " + grades_4);
+
+
+  }
+}
