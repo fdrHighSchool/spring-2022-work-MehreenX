a58a36071d91f4aba6389645028f33126de47dff
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-02-11 08:47:35 -0500

    ≥.

--- 01-StoreData/Student.java
+++ 01-StoreData/Student.java
@@ -4,4 +4,47 @@ private String id;
 private String name;
 private boolean closeContact;
 private int [] grades;
-}
+
+//constructor method
+public student(String id, String
+ n, boolean cc, int[] g){
+  tpublic double getAverage() {
+    int total = 0;
+
+    for (int grade : this.grades) {
+      total += grade;
+    } // end for-each loop
+    return total / grades.length;
+  } // end getAverage method
+
+//behaviors (methods)
+
+//get the close closeContact status of thr student
+public boolean isCloseContact(){
+
+}//end isCloseContact
+
+public void setCloseContact(boolean value) {
+
+}//end setCloseContact
+
+
+//see if student is failing
+public boolean isFailing(){
+
+
+}//end isFailing
+
+public String getName(){
+
+}//end getName
+
+public double getAverage() {
+    int total = 0;
+
+    for (int grade : this.grades) {
+      total += grade;
+    } // end for-each loop
+    return total / grades.length;
+  } // end getAverage method
+}//end class
