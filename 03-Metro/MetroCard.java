e9cf4c26bd23a54a87a3302f516647c1aa6681c2
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-02-18 08:46:35 -0500

    hw for break

---
+++ 03-Metro/MetroCard.java
@@ -0,0 +1,6 @@
+public class MetroCard{
+ public static void main(String[] args) {
+   String name1 = "Anna";
+   Int numUses = 3;
+ }
+}
---
+++ 03-Metro/Tester.java
@@ -0,0 +1,13 @@
+public class Tester{
+
+  private String name;
+  private int numUses;
+
+  public Tester(String n, int nu){
+    this.n = n
+    this.nu = nu
+
+
+  }
+
+}
