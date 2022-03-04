edbda79795e846efaa54f55592ed53d652b1eb7b
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-03-04 08:47:08 -0500

    still needs works

---
+++ 2019/StepTracker.java
@@ -0,0 +1,27 @@
+public class StepTracker{
+
+  private int activeDays;
+  private int totalDays;
+  private int totalSteps;
+  private int MinSteps;
+
+  public StepTracker(int MinSteps){
+    this.activeDays = 0;
+    this.totalDays = 0;
+    this.totalSteps = 0;
+    this.MinSteps = MinSteps;
+
+  }//end StepTracker
+
+  public void addDailySteps(){
+    //int Steps = numSteps;
+    //if (steps >= MinSteps ) {
+      //return "Your Average Steps daily were active!";
+  //  }//end if
+    //else {
+    //  return "Your Average Steps daily were NOT active!";
+    //}//end else
+
+  //}//end addDailySteps
+
+//}//end class
