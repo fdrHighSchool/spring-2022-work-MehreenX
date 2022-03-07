285cca3ab98ca17b6375ff0b7241306dbfa0e0fd
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-03-07 00:05:08 -0500

    updating...

---
+++ 03-Rational/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String args[]){
+
+    Rational rational1 = new Rational(1,2);
+    Rational rational2 = new Rational(1,2);
+    System.out.println(rational1.add(rational2));
+    
+  }
+  
+}
