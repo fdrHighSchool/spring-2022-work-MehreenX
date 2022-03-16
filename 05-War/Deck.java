a1c7da6083a1b23bcf45ead212bc5c1ffaadebf9
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-03-16 08:47:46 -0400

    :p

--- 05-War/Deck.java
+++ 05-War/Deck.java
@@ -16,6 +16,17 @@ public class Deck {
 
   public int getLength() {
     return deck.size();
+  }// end getLength
+
+  public void shuffle(){
+    //Method 1
+    ArrayList<Card> shuffledDeck = New ArrayList<Card>();
+    for(int i = 0; i < 52; i++){
+      for(int value)
+    }
+    int r = (int)(Math.random() * this.deck.size() - 1));
+    shuffledDeck.add(this.deck.get(r));
+    this.deck.remove(r);
   }
 
   public void displayDeck() {
