000f7eb2afdd0be790cdfcbce059983286d01d21
MehreenX <91324906+MehreenX@users.noreply.github.com>
2022-03-16 00:07:50 -0400

    Card pile...

--- 05-War/Pile.java
+++ 05-War/Pile.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class Pile {
 
-}
+  private int ArrayList<Card> pile; //ArrayList of Cards  to store
+
+  public CardPile(){
+    pile = new ArrayList<Card>();
+  }//end CardPile
+
+  public void addCard(C){
+    pile.add(C); // puts a Card at the end ("bottom") of the pile.
+  }//end addCard
+
+  public Card getTopCard(){
+    return pile.remove(0); //removes and returns the "top" card of the pile.
+  }//end getTopCard
+
+
+
+
+
+
+
+}//end calss
